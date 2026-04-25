c9964b1bf4f932977ccc5868fe467085a6c0f96b
Nithish2362 <kumarnithish941@gmail.com>
2026-04-25 17:27:35 +0530

    added new featuresd

---
+++ src/test/java/pg/pg/PgApplicationTests.java
@@ -0,0 +1,13 @@
+package pg.pg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
