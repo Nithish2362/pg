d309b4bdba17c67dcedcb79e498e04beb5b89d8c
Nithish2362 <kumarnithish941@gmail.com>
2026-04-26 13:29:48 +0530

    new

--- src/main/java/pg/pg/repository/FeaturesRepository.java
+++
@@ -1,7 +0,0 @@
-package pg.pg.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pg.pg.model.Features;
-
-public interface FeaturesRepository extends JpaRepository<Features, String> {
-}
--- src/main/java/pg/pg/repository/FloorRepository.java
+++
@@ -1,9 +0,0 @@
-package pg.pg.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pg.pg.model.Floor;
-import java.util.List;
-
-public interface FloorRepository extends JpaRepository<Floor, Long> {
-    List<Floor> findByBuildingId(Long buildingId);
-}
